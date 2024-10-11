9b9c6f91fee47a8c60ebd29e17c67cc0d1311e71
Oksana Bondareva <oksanchik252@gmail.com>
2024-10-12 01:07:56 +0300

    feat: add up finction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readline from 'readline/promises';
 import { stdin, stdout } from 'process';
 import { homedir } from 'os';
+import up from './up/up.js';
 
 const rl = readline.createInterface({
     input: stdin,
@@ -9,7 +10,23 @@ const rl = readline.createInterface({
 
 const usernameArg = process.argv.find(arg => arg.startsWith('--username='));
 const username = usernameArg ? usernameArg.split('=')[1] : 'Anonymous';
-const currentDir = homedir();
+let currentDir = homedir();
 
 rl.write(`Welcome to the File Manager, ${username}!\n`);
 rl.write(`You are currently in ${currentDir}\n`);
+
+rl.on('line', async (input) => {
+  const [command, ...args] = input.trim().split(' ');
+
+  switch (command) {
+      case 'up':
+          currentDir = up(currentDir);
+          rl.write(`You are currently in ${currentDir}\n`);
+          break;
+      case '.exit':
+          rl.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
+          process.exit(0);
+  }
+}).on('close', () => {
+  rl.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
+});
